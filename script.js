4f4b34ac9f9d261fecc7a11764e39b038b2941ea
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2022-11-09 22:05:47 +0530

    Word capture feature implemented using UI kb

--- script.js
+++ script.js
@@ -7,30 +7,59 @@ let clearbtn = document.querySelector("#my-clear-btn");
 let rowCntr = counter(0); // Row counter initislised
 let colCntr = counter(0); // Column counter also initialised
 
-function rowWiseDataEntry(data, rowNum, colNum) {
-    gridRows[rowNum].children[colNum].textContent = data;
-}
-
 kbModule.onclick = function (e) {
-    //console.log(test(e));
+    let key = clickedKey(e);
+
     if (rowCntr.currCount() < 6) {
-        if (e.target.innerText !== "ENTER" && colCntr.currCount() < 5) {
-            rowWiseDataEntry(
-                e.target.innerText,
-                rowCntr.currCount(),
-                colCntr.currCount()
-            );
+        if (key !== "ENTER" && colCntr.currCount() < 6) {
+            if (key == "clear" && colCntr.currCount() >= 0) {
+                colCntr.decCount();
+                clearCellData(rowCntr.currCount(), colCntr.currCount());
+
+                if (colCntr.currCount() >= 0) {
+                    colCntr.decCount();
+                }
+            } else {
+                enterCellData(key, rowCntr.currCount(), colCntr.currCount());
+            }
+
             if (colCntr.currCount() != 5) {
                 colCntr.incCount();
             }
-        } else if (e.target.innerText == "ENTER") {
+        } else if (key == "ENTER") {
             if (colCntr.currCount() == 5) {
                 rowCntr.incCount();
                 colCntr = counter(0);
+            } else {
+                alert("word is short");
             }
         }
     }
 };
+
+//function to enter cell data in a row
+function enterCellData(data, rowNum, colNum) {
+    if (colNum > -1 && colNum < 5) {
+        gridRows[rowNum].children[colNum].textContent = data;
+    }
+}
+
+//function to clear the cell data
+function clearCellData(rowNum, colNum) {
+    if (colNum > -1 && colNum < 5) {
+        gridRows[rowNum].children[colNum].textContent = "";
+    }
+}
+
+//function to find the clicked key
+function clickedKey(e) {
+    //console.log(ev.target.innerText);
+    if (e.target.innerText != "") {
+        return e.target.innerText;
+    } else {
+        return "clear";
+    }
+}
 
 //Counter inplementation for accessing Rows and Columns of grid
 function counter(initialVal) {
