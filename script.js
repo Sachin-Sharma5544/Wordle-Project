3229f73a2a3ec7c6e1f2ee7a686b72bbd403c6b6
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2022-11-09 19:51:02 +0530

    js file updated, clear btn html and styles added

--- script.js
+++ script.js
@@ -1,60 +1,55 @@
-//Counter inplementation for accessing Rows and Columns of grid
-function counter(initialVal) {
-    let count = initialVal;
-    function incrementCount() {
-        count++;
-        return count;
-    }
-    function decrementCount() {
-        count--;
-        return count;
-    }
-
-    function currentCountVal() {
-        return count;
-    }
-    return {
-        incrementCount: incrementCount,
-        decrementCount: decrementCount,
-        currentCountVal: currentCountVal,
-    };
-}
-
-let rowCounter = counter(0); // Row counter initislised
-let colCounter = counter(0); // Column counter also initialised
-
 // let secretWord = ["B", "E", "G", "I", "N"];
 
 let kbModule = document.querySelector(".my-kb-row-wrapper");
 let gridRows = document.querySelectorAll(".my-word-grid");
+let clearbtn = document.querySelector("#my-clear-btn");
+
+let rowCntr = counter(0); // Row counter initislised
+let colCntr = counter(0); // Column counter also initialised
+
+function rowWiseDataEntry(data, rowNum, colNum) {
+    gridRows[rowNum].children[colNum].textContent = data;
+}
 
 kbModule.onclick = function (e) {
-    //console.log(e);
-    if (rowCounter.currentCountVal() < 6) {
-        if (
-            e.target.innerText !== "ENTER" &&
-            colCounter.currentCountVal() < 5
-        ) {
-            gridRows[rowCounter.currentCountVal()].children[
-                colCounter.currentCountVal()
-            ].textContent = e.target.innerText;
-
-            if (colCounter.currentCountVal() != 4) {
-                colCounter.incrementCount();
+    //console.log(test(e));
+    if (rowCntr.currCount() < 6) {
+        if (e.target.innerText !== "ENTER" && colCntr.currCount() < 5) {
+            rowWiseDataEntry(
+                e.target.innerText,
+                rowCntr.currCount(),
+                colCntr.currCount()
+            );
+            if (colCntr.currCount() != 5) {
+                colCntr.incCount();
             }
         } else if (e.target.innerText == "ENTER") {
-            if (colCounter.currentCountVal() == 4) {
-                rowCounter.incrementCount();
-                colCounter = counter(0);
+            if (colCntr.currCount() == 5) {
+                rowCntr.incCount();
+                colCntr = counter(0);
             }
         }
     }
 };
 
-/*
-//This is the hardcoded implementation of the wordle application
-//tomorrow i have implement it in generalized form
-console.log(gridRows[0].children[1]);
-console.log(gridRows[0].children[0]);
+//Counter inplementation for accessing Rows and Columns of grid
+function counter(initialVal) {
+    let count = initialVal;
+    function incCount() {
+        count++;
+        return count;
+    }
+    function decCount() {
+        count--;
+        return count;
+    }
 
-*/
+    function currCount() {
+        return count;
+    }
+    return {
+        incCount: incCount,
+        decCount: decCount,
+        currCount: currCount,
+    };
+}
